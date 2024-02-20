8a0e9aeef3b85e387ba68420c2834a46833109e3
Dwarakeshwaran Mageshkumar <sumandsm00@gmail.com>
2024-02-20 12:35:42 -0500

    dbudgeteer Spring Boot App Added

---
+++ src/main/java/dwaki/dbudgeteer/DbudgeteerApplication.java
@@ -0,0 +1,15 @@
+package dwaki.dbudgeteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbudgeteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbudgeteerApplication.class, args);
+		
+		System.out.println("Check");
+	}
+
+}
---
+++ src/test/java/dwaki/dbudgeteer/DbudgeteerApplicationTests.java
@@ -0,0 +1,13 @@
+package dwaki.dbudgeteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbudgeteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
